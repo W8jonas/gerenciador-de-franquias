0a37ae6a98aa2d56d954641968ba14fa9ce0b79b
Mario Cesario <mario.henrique.cesario@gmail.com>
2025-07-27 20:40:10 -0300

    feat: estrutura inicial do projeto com tela de login, classe Owner e teste unitário

---
+++ main/java/models/User.java
@@ -0,0 +1,23 @@
+package models;
+
+public abstract class User {
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+}
