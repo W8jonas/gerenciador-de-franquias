7af840d359db2cb2dee5da3dbc82fb89bb797b50
W8jonas <jonashenrique989@gmail.com>
2025-07-27 21:51:41 -0300

    first commit

---
+++ src/main/java/models/Owner.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Owner extends User {
+    public Owner(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
---
+++ src/test/java/models/OwnerTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OwnerTest {
+    @Test
+    public void testAuthenticate() {
+        Owner owner = new Owner("Admin", "admin@company.com", "123");
+        assertTrue(owner.authenticate("123"));
+        assertFalse(owner.authenticate("wrong"));
+    }
+}
